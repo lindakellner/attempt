f45c192f68d907881ca3a5c927bc4efd08734c0e
linda <linda.kellner@htp-tel.de>
2016-08-31 08:23:16 +0200

    second

---
+++ pin_78.ino
@@ -0,0 +1,15 @@
+  void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);  
+}
+
+  void loop() {
+  digitalWrite(8, HIGH);
+  delay(1000);              
+  digitalWrite(7, HIGH);    
+  delay(1000); 
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(7, LOW);
+  delay(1000);
+ }
