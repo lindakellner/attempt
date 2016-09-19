0b67a0b6ec62235cbde82b6321b81a29d3d7d09b
lindakellner <linda.kellner@htp-tel.de>
2016-09-19 12:48:22 +0200

    Add files via upload

---
+++ dreifachblinker_verk_rzt.ino
@@ -0,0 +1,46 @@
+const int ledPin1 = 8;
+const int ledPin2 = 10;
+const int ledPin3 = 12;
+
+int ledState1 = LOW;
+int ledState2 = LOW;
+int ledState3 = LOW;
+
+unsigned long previousMillis1 = 0;
+unsigned long previousMillis2 = 0;
+unsigned long previousMillis3 = 0;
+
+int led1int = 500;
+int led2int = 1000;
+int led3int = 1500;
+ 
+void setup() {
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);  
+  pinMode(ledPin3, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+if (currentMillis - previousMillis1 >= led1int) {
+  previousMillis1 = currentMillis;
+  ledState1=!ledState1;
+}
+
+  if (currentMillis - previousMillis2 >= led2int) {
+    previousMillis2 = currentMillis;  
+    ledState2=!ledState2;
+}
+
+    if (currentMillis - previousMillis3 >= led3int) {
+    previousMillis3 = currentMillis;
+    ledState3=!ledState3;
+}
+
+  digitalWrite(ledPin1, ledState1);
+  digitalWrite(ledPin2, ledState2);
+  digitalWrite(ledPin3, ledState3);
+  }  
+
+
+
