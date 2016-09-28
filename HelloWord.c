27a5c6fd67e319038de876591c719cd0f54f12f4
linda <linda.kellner@htp-tel.de>
2016-09-28 16:48:27 +0200

    Add line counter and file creator

---
+++ HelloWord.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    long long foo = 1;
+    while(1)
+    {
+      printf("%lli: HEY LINDAjseklfnsejfsfsfsfsfsfffsfsdfsfsfsdfsdfsfsdfsdfsfsdfsfsdfk\n", foo);
+      ++foo;
+    }
+    return 0;
+}
---
+++ line.py
@@ -0,0 +1,3 @@
+num_lines = sum(1 for line in open('data.txt'))
+print(num_lines)
+input()
